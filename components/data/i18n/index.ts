21918598b37a975b88df2296a4688ef63f736ce8
Stefano Marodin <stefano@marodin.it>
2022-03-17 17:45:51 +0100

    francese

--- components/data/i18n/index.ts
+++ components/data/i18n/index.ts
@@ -1,9 +1,10 @@
 import i18n, { use } from "i18next"
 import translationEN from "public/static/locales/en/common.json"
 import translationIT from "public/static/locales/it/common.json"
+import translationFR from "public/static/locales/fr/common.json"
 import { initReactI18next } from "react-i18next"
 
-const languages = ["en", "it"]
+const languages = ["en", "it", "fr"]
 
 const resources = {
   en: {
@@ -12,6 +13,9 @@ const resources = {
   it: {
     translation: translationIT,
   },
+  fr: {
+    translation: translationFR,
+  }
 }
 
 use(initReactI18next).init({
